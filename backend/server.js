47470bc2fabba4f547da664dc51f6c6a6a362a2a
naresh-2026 <mannamnaresh9@gmail.com>
2025-08-16 03:38:40 +0530

    solved bug

--- backend/server.js
+++ backend/server.js
@@ -14,11 +14,11 @@ const port = 3001;
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use(express.static(path.join(__dirname, 'build')));
+ app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'build', 'App.js'));
+// });
 
 const mongoURI = process.env.MONGO_URI;
 
